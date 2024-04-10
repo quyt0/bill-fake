bb369c4d094cbc575b427fbe51f63966607071c8
quyt0 <62830346+quyt0@users.noreply.github.com>
2024-04-10 12:13:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,25 @@ function getRandomInt(min, max) {
 function addZero(i) {
     if (i < 10) {i = "0" + i}
     return i;
-  }
+}
+
+function removeVietnameseTones (str) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a')
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e')
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o')
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u')
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+  str = str.replace(/đ/g, 'd')
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, 'A')
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, 'E')
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, 'I')
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, 'O')
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, 'U')
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, 'Y')
+  str = str.replace(/Đ/g, 'D')
+  return str
+}
 
 function Create() {
     var canvas = document.getElementById("canvas");
@@ -89,7 +107,7 @@ function Create() {
         context.textAlign = "end";
         context.font = "39px averta_std_cybold";
 	var nameid = getRandomInt(0, 8754);
-	var namestr = namedat[nameid].full_name.toString();
+	var namestr = removeVietnameseTones(namedat[nameid].full_name).toString().toUpperCase();
         context.fillText(namestr, 1020, 1150);
         context.font = "39px averta_std_cyregular";
         context.fillText(namestr, 1020, 1230);
